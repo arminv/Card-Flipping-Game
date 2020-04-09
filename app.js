3736822cb68a47a29ba9564465a2dcc4164164f6
arminv <arminv@users.noreply.github.com>
2020-04-09 00:11:46 -0400

    Added timer. Improved styling

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
+// TODO: Add a tracker to see how many attempts it took to win.
+
 const NUMBER_OF_PAIRS = 8;
 const DEFAULT_IMG = './img/default.png';
-// Number of clicks:
-let numClick = 0;
-// Values of clicks
-let clickValues = [];
-// This will be used for finding matched cards:
-let myState = [3, 4, 3, 8, 6, 4, 8, 7, 2, 7, 5, 5, 2, 1, 6, 1];
-// Number of card pairs matched:
-let myWins = 0;
 
-// TODO: Add a timer to see how long it takes the user to finish the game:
+// Number of clicks:
+var numClick = 0;
+// Values of clicks:
+var clickValues = [];
+// This will be used for finding matched cards:
+var gameState = [3, 4, 3, 8, 6, 4, 8, 7, 2, 7, 5, 5, 2, 1, 6, 1];
+// Number of card pairs matched:
+var numberOfWins = 0;
+var firstGame = true;
 
 const resetState = () => {
     numClick = 0;
@@ -22,7 +24,7 @@ const resetImages = () => {
 }
 
 const shuffle = array => {
-    let currentIndex = array.length,
+    var currentIndex = array.length,
         temporaryValue, randomIndex;
     // While there remain elements to shuffle...
     while (0 !== currentIndex) {
@@ -37,24 +39,28 @@ const shuffle = array => {
     return array;
 }
 
-shuffle(myState);
+shuffle(gameState);
 
-const myNewGame = () => {
+const newGame = () => {
+    numberOfWins = 0;
+    restartTimer();
     resetState();
-    myWins = 0;
-    shuffle(myState);
+    shuffle(gameState);
     for (i = 1; i < 17; i++) {
         document.getElementById(i.toString()).style.visibility = 'visible';
         document.getElementById(i.toString()).src = DEFAULT_IMG;
     }
 }
 
-const myReplace = original => {
+const runGame = original => {
+    if (firstGame) {
+        startTimer();
+    }
     clickValues.push(original);
 
     if (numClick == 0) {
         var elOne = document.getElementById(original.toString());
-        elOne.src = './img/' + myState[clickValues[numClick] - 1].toString() + '.jpg';
+        elOne.src = './img/' + gameState[clickValues[numClick] - 1].toString() + '.jpg';
         numClick++;
 
     } else if (numClick == 1) {
@@ -62,27 +68,81 @@ const myReplace = original => {
             alert('Choose a different card please!');
             resetImages();
             resetState();
-        }
-
-        var elTwo = document.getElementById(original.toString());
-        elTwo.src = './img/' + myState[clickValues[numClick] - 1].toString() + '.jpg';
-        numClick++;
-
-        if (myState[clickValues[0] - 1] === myState[clickValues[1] - 1]) {
-            document.getElementById(clickValues[0].toString()).style.visibility = 'hidden';
-            document.getElementById(clickValues[1].toString()).style.visibility = 'hidden';
-            resetState();
-            myWins++;
         } else {
-            setTimeout(resetImages, 500);
-            setTimeout(resetState, 500);
+            var elTwo = document.getElementById(original.toString());
+            elTwo.src = './img/' + gameState[clickValues[numClick] - 1].toString() + '.jpg';
+            numClick++;
+
+            if (gameState[clickValues[0] - 1] === gameState[clickValues[1] - 1]) {
+                document.getElementById(clickValues[0].toString()).style.visibility = 'hidden';
+                document.getElementById(clickValues[1].toString()).style.visibility = 'hidden';
+                resetState();
+                numberOfWins++;
+            } else {
+                setTimeout(resetImages, 500);
+                setTimeout(resetState, 500);
+            }
         }
     }
-    if (myWins >= NUMBER_OF_PAIRS) {
-        if (confirm("Game is Over! Would you like to play again?")) {
-            myNewGame();
+    if (numberOfWins >= NUMBER_OF_PAIRS) {
+        if (confirm(`Game is Over! Your Time Was: ${stopwatchEl.innerHTML}. Would you like to play again?`)) {
+            newGame();
         } else {
+            pauseTimer();
             return;
         }
     }
 };
+
+// -----------------Stopwatch-----------------
+// https://www.youtube.com/watch?v=oY8V6GuZrkM
+
+var milliseconds = 0,
+    seconds = 0,
+    minutes = 0;
+var timer;
+
+const stopwatchEl = document.querySelector('.timer');
+
+const startTimer = () => {
+    if (!timer) {
+        timer = setInterval(runTimer, 10);
+    }
+}
+
+const runTimer = () => {
+    stopwatchEl.innerHTML = getTimer();
+    milliseconds++;
+    if (milliseconds == 100) {
+        milliseconds = 0;
+        seconds++;
+    }
+    if (seconds == 60) {
+        seconds = 0;
+        minutes++;
+    }
+}
+
+const pauseTimer = () => {
+    clearInterval(timer);
+    timer = false;
+}
+
+const stopTimer = () => {
+    clearInterval(timer);
+    timer = false;
+    milliseconds = 0;
+    seconds = 0;
+    minutes = 0;
+    stopwatchEl.innerHTML = getTimer();
+}
+
+const restartTimer = () => {
+    stopTimer();
+    startTimer();
+}
+
+const getTimer = () => {
+    return (m < 10 ? '0' + m : m) + ':' + (s < 10 ? '0' + s : s) + ':' + (ms < 10 ? '0' + ms : ms);
+}
+// -------------------------------------------
